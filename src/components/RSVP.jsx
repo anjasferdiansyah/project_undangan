216c90930c8041eca0311d40d04863c329adf33f
anjasferdiansyah <anjasferdiansyah123@gmail.com>
2024-06-09 23:37:45 +0700

    first commit

---
+++ src/components/RSVP.jsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import React from "react";
+
+const RSVP = () => {
+  return (
+    <div className="bg-[#B7B1A4]">
+      <div className="container px-4">
+        <div className="h-[280px] pt-8">
+          <Image
+            src="/img/22.jpg"
+            width={500}
+            height={500}
+            className="rounded-tl-[40%] rounded-br-[40%] object-cover w-full h-full"
+            alt=""
+          />
+        </div>
+        <h1 className="text-2xl text-white font-playfair italic pb-4">RSVP</h1>
+        <p className="text-white text-xs">
+          Kirimkan Konfirmasi Kehadiran Kepada Mempelai Melalui Form Berikut.
+        </p>
+        <form action="" className="py-8 space-y-2">
+          <input
+            type="text"
+            name=""
+            className="w-full p-2 rounded"
+            placeholder="Nama Lengkap"
+            id=""
+          />
+          <input
+            type="number"
+            name=""
+            placeholder="1"
+            className="w-full p-2 rounded"
+            id=""
+          />
+          <button className="w-full p-2 rounded border-2 border-[#eeebe4] bg-white text-[#B7B1A4]">
+            Kirim
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RSVP;
